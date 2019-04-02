2ff7cf1a4634f66a4b999b92c317ae0861b1160f
Ryan Kadri <ryankadri1@gmail.com>
2019-04-02 15:31:26 -0400

    Cors headers for SAM template

--- packages/games-backend/src/calc-next-move.py
+++ packages/games-backend/src/calc-next-move.py
@@ -8,4 +8,10 @@ from games.tic_tac_toe import calcNextMove
 def lambda_handler(event, context):
     board = json.loads(event["body"])
     nextMove = calcNextMove(board)
-    return json.dumps(nextMove)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(nextMove)
+    }
