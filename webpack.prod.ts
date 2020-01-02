293f45e7a27c5d45ae81dd49860de30e6fcc772f
johndoe@example.com <johndoe@example.com>
2020-01-01 22:18:39 -0500

    Test no plugins

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,16 +1,15 @@
 import webpack from "webpack";
-import bundle from 'webpack-bundle-analyzer';
 import merge from 'webpack-merge';
 import common from './webpack.common';
 
 
 const config: webpack.Configuration = merge(common, {
     mode: 'production',
-    plugins: process.env.CICD 
-        ? []
-        : [
-            new bundle.BundleAnalyzerPlugin()
-        ]
+    // plugins: process.env.CICD 
+    //     ? []
+    //     : [
+    //         new bundle.BundleAnalyzerPlugin()
+    //     ]
 });
 
 export default config;
