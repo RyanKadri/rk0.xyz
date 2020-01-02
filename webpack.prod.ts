92cecf9d949f9b15badcf2b7dfca5e16f15dd959
johndoe@example.com <johndoe@example.com>
2020-01-01 22:44:13 -0500

    Hide package lock?

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,15 +1,16 @@
 import webpack from "webpack";
+import bundle from 'webpack-bundle-analyzer';
 import merge from 'webpack-merge';
 import common from './webpack.common';
 
 
 const config: webpack.Configuration = merge(common, {
     mode: 'production',
-    // plugins: process.env.CICD 
-    //     ? []
-    //     : [
-    //         new bundle.BundleAnalyzerPlugin()
-    //     ]
+    plugins: process.env.CICD 
+        ? []
+        : [
+            new bundle.BundleAnalyzerPlugin()
+        ]
 });
 
 export default config;
