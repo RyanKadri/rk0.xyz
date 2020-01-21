5d2a29fb9f8d12f95c69242d57c6bdb5f3e139c2
Ryan Kadri <ryankadri1@gmail.com>
2020-01-21 08:21:39 -0500

    Lesson 2

---
+++ packages/site/src/lessons/sw-construction/lesson2/index.ts
@@ -0,0 +1,23 @@
+import { Agenda, IntroJvm, IntroJvmClassLoading, InversionOfControl, JavaHasMagic, Maven, PackageManagementJava, PackageManagementNode1, PackageManagementNode2, ProjectManagement, References, Title } from "./slides/simple";
+
+export const lesson2 = {
+    title: "The jar as a door",
+    description: "Managing Dependencies. Introducing Spring",
+    slug: "dependencies-spring",
+    slides: [
+        Title,
+        Agenda,
+        IntroJvm,
+        IntroJvmClassLoading,
+        JavaHasMagic,
+        ProjectManagement,
+        PackageManagementNode1,
+        PackageManagementNode2,
+        PackageManagementJava,
+        Maven, 
+        InversionOfControl,
+        References
+    ],
+    examples: [
+    ]
+}
