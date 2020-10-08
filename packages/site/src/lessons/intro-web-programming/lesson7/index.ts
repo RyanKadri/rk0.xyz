b464bc798cd4e4be4e320205ed4677f5d74104c8
johndoe@example.com <johndoe@example.com>
2020-10-07 23:15:01 -0400

    Recording

--- packages/site/src/lessons/intro-web-programming/lesson7/index.ts
+++ packages/site/src/lessons/intro-web-programming/lesson7/index.ts
@@ -25,5 +25,7 @@ export const lesson7: Presentation = {
     examples: [
         
     ],
-    recording: undefined
+    recording: {
+        link: "https://temple.zoom.us/rec/play/7z6KWMNOLDI-ox_zXYMQ-IIgTdb2KWoIQqVeaVDdy9zWUg_FMMkJimtbranbeHlsirr09RXYdGLR8Nn0.-M_8PVSA8xCx275l?autoplay=true&startTime=1602105988000"
+    }
 }
