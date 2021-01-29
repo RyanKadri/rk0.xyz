fd625c8255e042806d3bc23bda0005f2bb4b5634
Ryan Kadri <ryankadri1@gmail.com>
2021-01-29 03:50:34 +0000

    Class recording

--- packages/site/src/lessons/sw-construction/lesson2/index.ts
+++ packages/site/src/lessons/sw-construction/lesson2/index.ts
@@ -1,9 +1,10 @@
+import { Presentation } from "../../../../../presenter-core/src/services/types";
 import { generateReferencesSlide } from "../../shared/references-slide";
 import { JSONExample } from "./slides/json";
 import { SemanticVersioning } from "./slides/semantic-versioning";
 import { DependencyDive, DistributedSystems, DistributedSystemsApproaches, DistributedSystemsInClass, EnterPackageManagers, ExampleTime, InversionOfControl, Maven, MavenDependencies, NaiveDownsides, PackageManagementJava, PartTwo, PossibilitiesForOpenSource, ProjectManagement, references, Spring, SpringCode, Title, WhatIsAJavaLibrary } from "./slides/simple";
 
-export const lesson2 = {
+export const lesson2: Presentation = {
     title: 'The jar is a "door"',
     description: "Managing Dependencies. Introducing Spring",
     slug: "dependencies-spring",
@@ -31,5 +32,8 @@ export const lesson2 = {
         generateReferencesSlide(references)
     ],
     examples: [
-    ]
+    ],
+    recording: {
+        link: "https://temple.zoom.us/rec/play/Eydnbvf4KcXvQrzhZrHy3NdZmR3qXRS0U0PuMleRvsW8kyANVEy1Nb8tgr5FTRwU864_m_0CB8nLCAWO.WoQlXN2kU6g4Dga3?autoplay=true&startTime=1611786459000"
+    }
 }
