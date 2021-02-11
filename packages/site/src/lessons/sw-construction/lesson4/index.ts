c023db578bcb040fb80977e00a62f3c3f70b0cea
Ryan Kadri <johndoe@example.com>
2021-02-10 22:35:55 -0500

    Recording link

--- packages/site/src/lessons/sw-construction/lesson4/index.ts
+++ packages/site/src/lessons/sw-construction/lesson4/index.ts
@@ -1,7 +1,8 @@
+import { Presentation } from "../../../../../presenter-core/src/services/types";
 import { generateReferencesSlide } from "../../shared/references-slide";
 import { AssignmentTestExample, BadUnitTestExample, BddTesting, ClassQuestion, CucumberDependencies, CucumberTesting, FunctionalTests, IntegrationTests, JUnitAnnotations, JUnitAssertions, JUnitTesting, ManualTestingApproach, ManualTestingDownsides, references, TddTesting, TestingPortions, TestingPyramid, Title, TypesOfTests, UnitTests, WriteAnExpressionEngine } from "./slides/simple";
 
-export const lesson4 = {
+export const lesson4: Presentation = {
     title: 'Automated Testing',
     description: "Using JUnit and Cucumber to test your code",
     slug: "automated-testing",
@@ -30,5 +31,8 @@ export const lesson4 = {
     ],
     references,
     examples: [
-    ]
+    ],
+    recording: {
+        link: "https://temple.zoom.us/rec/play/Q6CeWvkC1hu_rNhscHvh73kYEh_z_cQMlcnbv2deZ0ZFeyvkrOWpi-T7FIAJzy_EZl4HslDYJ_OHhDZD.-F0MnU-rOX8pa1I7?autoplay=true&startTime=1612995985000"
+    }
 }
