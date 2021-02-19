a950161bcc915a73c2eea5d1b7befe13064d42ef
Ryan Kadri <johndoe@example.com>
2021-02-19 17:52:11 -0500

    Class recording

--- packages/site/src/lessons/sw-construction/lesson5/index.ts
+++ packages/site/src/lessons/sw-construction/lesson5/index.ts
@@ -1,7 +1,8 @@
+import { Presentation } from "../../../../../presenter-core/src/services/types";
 import { CarefulWithReferences, CaseStudyGetters, CaseStudySetters, ConstructorsAreBetter, DataIsDangerous, FunctionalJava, FunctionalJava2, FunctionalProgramming1, FunctionalProgrammingIntro, IsAPureFunction, NotAPureFunction, PossibleLocations, PureFunctions, ReducingState, Title, UnsafeSharing, UseDataRightAway, UseDataRightAway2, UseLocals, WhereData } from "./slides/simple";
 import { VariablesAsLabels } from "./slides/variables-as-labels";
 
-export const lesson5 = {
+export const lesson5: Presentation = {
     title: 'Putting the fun in functional',
     description: "Programming Best Practices: Functional and Immutable Programming",
     slug: "functional-immutable",
@@ -29,5 +30,8 @@ export const lesson5 = {
         FunctionalJava2,
     ],
     examples: [
-    ]
+    ],
+    recording: {
+        link: "https://temple.zoom.us/rec/play/2kaKpoF4SNS-nhl9JhKp1aBrXD7wMBW3jrAbtc9jJPMizw2rr0uoN1JGXQkwWbRNUMQuFm_J6dKrcPO5.9ieBiB1_8V0cZ8LT?continueMode=true"
+    }
 }
