52925770698cf05c9782e9b7e5bdccca3495e3a6
Ryan Kadri <johndoe@example.com>
2021-03-16 22:49:54 -0400

    Resiliency Lecture

---
+++ packages/site/src/lessons/sw-construction/lesson8/index.ts
@@ -0,0 +1,35 @@
+import { Presentation } from "../../../../../presenter-core/src/services/types";
+import { generateReferencesSlide } from "../../shared/references-slide";
+import { AlertingIssues, Downtime, HowDoManage, HowDoWeFixIt2, HowToFix, JavaLogging, Logging, ManageLaunchWithAws, ManageLoadBalancer, ManageScalingWithAws, ManageTargetGroups, Nines, OnAws, references, SplittingUpAService, Title, WhatCanGoWrong2, WhatCanGoWrongQuestion } from "./slides/simple";
+
+export const lesson8: Presentation = {
+    title: 'Resiliency, Logging, and Monitoring',
+    description: "Deploying reliable services (and picking them up when they fall down)",
+    slug: "resiliency",
+    slides: [
+        Title,
+        WhatCanGoWrongQuestion,
+        Downtime,
+        Nines,
+        HowToFix,
+        WhatCanGoWrong2,
+        HowDoWeFixIt2,
+        SplittingUpAService,
+        HowDoManage,
+        OnAws,
+        ManageLaunchWithAws,
+        ManageScalingWithAws,
+        ManageTargetGroups,
+        ManageLoadBalancer,
+        AlertingIssues,
+        Logging,
+        JavaLogging,
+        generateReferencesSlide(references)
+    ],
+    examples: [
+    ],
+    recording: {
+        link: "https://temple.zoom.us/rec/play/2D7vi7aFO6KItPW3ajWdV5MmD6AP872qi3Zm4zwKkwR895LMHkK33y-9RHYGrEbyW9kWRHeenHMqSik.Ca00UBvBwHALwP5-?continueMode=true",
+    },
+    references
+}
