32ded0398248a814a83b984be7f7fc75a31ee442
Ryan Kadri <johndoe@example.com>
2022-03-07 22:29:22 -0500

    Cloud

--- packages/site/src/lessons/practicum-s2022/index.ts
+++ packages/site/src/lessons/practicum-s2022/index.ts
@@ -7,6 +7,7 @@ import { lesson3 } from "./lesson3";
 import { lesson4 } from "./lesson4";
 import { lesson5 } from "./lesson5";
 import { lesson6 } from "./lesson6";
+import { lesson7 } from "./lesson7";
 
 export const practicumInSW2022: CourseDefinition = {
     title: "Practicum in Software Construction",
@@ -18,7 +19,8 @@ export const practicumInSW2022: CourseDefinition = {
         lesson3,
         lesson4,
         lesson5,
-        lesson6
+        lesson6,
+        lesson7
     ],
     icon: faHammer,
     courseExtras: [
